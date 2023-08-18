815f7422eedda983eb4d88e5c75fbc258aac0f79
pyureshka <polinapyura@mail.ru>
2023-08-18 16:54:00 +0900

    add: custom input

--- src/components/ui/cInput.vue
+++ src/components/ui/cInput.vue
@@ -1,1 +1,37 @@
-<template></template>
+<script setup>
+defineProps(["type"]);
+
+const typesPlaceholder = {
+  email: "your@email.com",
+};
+</script>
+
+<template>
+  <input :type="type" :placeholder="typesPlaceholder[type]" class="input" />
+</template>
+
+<style>
+.input {
+  padding: 1rem 2rem;
+  color: var(--color-primary);
+  border: none;
+
+  display: flex;
+  align-items: center;
+  flex: 1 0 0;
+  align-self: stretch;
+}
+
+.input:focus {
+  outline: none;
+}
+
+.input::placeholder {
+  opacity: 0.5;
+  color: #fff;
+}
+
+.input_dark {
+  background-color: var(--color-light-primary);
+}
+</style>
