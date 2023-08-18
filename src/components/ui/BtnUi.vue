8aedde3918fac9b5076fd67a25e3d66d04b80c24
pyureshka <polinapyura@mail.ru>
2023-08-18 13:58:37 +0900

    add: ui btn

---
+++ src/components/ui/BtnUi.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps(["title"]);
+</script>
+
+<template>
+  <button type="button" class="btn">{{ title }}</button>
+</template>
+
+<style>
+.btn {
+  padding: 1rem 2rem;
+  width: fit-content;
+  background-color: var(--color-light-grey);
+  color: var(--color-primary);
+  border: none;
+  text-align: center;
+  cursor: pointer;
+}
+.btn:hover {
+  background-color: #ebe8f4;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import Page from "./components/layout/Page.vue";
 import Wrapper from "./components/layout/Wrapper.vue";
 import SectionBlock from "./components/layout/SectionBlock.vue";
-import Content from "./components/layout/Content.vue";
+import Btn from "./components/ui/BtnUi.vue";
 import "./style.css";
 
 import App from "./App.vue";
@@ -11,4 +11,5 @@ createApp(App)
   .component("Page", Page)
   .component("Wrapper", Wrapper)
   .component("SectionBlock", SectionBlock)
+  .component("Btn", Btn)
   .mount("#app");
