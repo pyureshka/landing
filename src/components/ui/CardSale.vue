e3a0d60ea222be09aaa925abbf5d9ef5574993bd
pyureshka <polinapyura@mail.ru>
2023-08-18 13:58:52 +0900

    add: sale card

---
+++ src/components/ui/CardSale.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps(["product", "prefix", "postfix"]);
+</script>
+<template>
+  <div class="card-sale">
+    <img :src="product.photo" alt="" class="card-sale__img" />
+
+    <div class="card-sale__desc">
+      <h2 class="card-sale__name">
+        {{ product.name }}
+      </h2>
+
+      <span class="card-sale__price">
+        {{ prefix }}
+        {{ `${product.price}` }}
+        {{ postfix }}
+      </span>
+    </div>
+  </div>
+</template>
+<style>
+.card-sale {
+  cursor: pointer;
+  min-width: fit-content;
+
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.5rem;
+}
+.card-sale:hover {
+  background-color: #ebe8f4;
+}
+@media screen and (max-width: 1024px) {
+  .card-sale:hover {
+    background-color: unset;
+    color: unset;
+  }
+}
+
+.card-sale__img {
+  height: auto;
+  width: auto;
+
+  flex-shrink: 0;
+}
+
+.card-sale__desc {
+  display: flex;
+  flex-direction: column;
+}
+
+.card-sale__name {
+  font-size: var(--text-h2);
+  font-weight: 400;
+  margin: 0;
+}
+.card-sale__price {
+}
+</style>
--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -1,46 +1,90 @@
 <script setup>
 import CardFeature from "../components/ui/CardFeature.vue";
+import CardSale from "../components/ui/CardSale.vue";
+
 import { featureInfo } from "../staticData/HomePageData";
+import { products } from "../staticData/ProductData";
 </script>
 
 <template>
   <page>
     <wrapper>
-      <content>
-        <div class="content-feature">
-          <h1 class="content-feature__title">What makes our brand different</h1>
+      <section-block>
+        <div class="content">
+          <h1 class="content__title content__title_center">
+            What makes our brand different
+          </h1>
 
-          <div class="content-feature__main">
-            <card-feature v-for="item in featureInfo" :icon="item.icon">
+          <div class="content__main">
+            <card-feature
+              v-for="item in featureInfo"
+              :key="item.title"
+              :icon="item.icon"
+            >
               <template #title>{{ item.title }}</template>
               <template #content>{{ item.content }}</template>
             </card-feature>
           </div>
         </div>
-      </content>
+      </section-block>
+
+      <section-block>
+        <div class="content">
+          <h1 class="content__title">New ceramics</h1>
+
+          <div class="content__main content__main_overflow">
+            <card-sale
+              v-for="item in products"
+              :key="item.name"
+              :product="item"
+              prefix="£"
+            />
+          </div>
+
+          <btn title="View collection" class="btn_center" />
+        </div>
+      </section-block>
     </wrapper>
   </page>
 </template>
 
 <style>
-.content-feature {
+.content {
   display: flex;
   flex-direction: column;
   row-gap: 2rem;
 }
 
-.content-feature__title {
+.content__title {
   font-size: var(--text-h1);
   font-weight: 400;
 
-  align-self: center;
   margin: 0;
 }
 
-.content-feature__main {
+.content__title_center {
+  align-self: center;
+}
+
+.content__main {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
   row-gap: 2rem;
+}
+
+.content__main_overflow {
+  max-height: 500px;
+
+  overflow: hidden;
+}
+@media screen and (max-width: 700px) {
+  .content__main_overflow {
+    max-height: 1000px;
+  }
+}
+
+.btn_center {
+  align-self: center;
 }
 </style>
